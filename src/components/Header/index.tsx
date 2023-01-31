b418edc1f9eedf78c6cb455130fc12405eefb921
Matheus Andrade <2002matheus@gmail.com>
2023-01-31 10:28:20 -0300

    fix some bugs and add local.storage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,47 @@
-import * as S from "./styles";
+import * as S from './styles'
 
 interface Props {
-  themeToggle: () => void;
+	themeToggle: () => void
 }
 
 const Header = ({ themeToggle }: Props) => {
-  return (
-    <S.Container>
-      <S.RightSide>
-        <S.Logo to="/">W2w?</S.Logo>
-        <S.HeaderItem to="/search">
-          <S.HeaderName>Pesquisa</S.HeaderName>
-          <S.SearchIcon />
-        </S.HeaderItem>
-      </S.RightSide>
-      <S.Root>
-        <S.Trigger>
-          <S.MenuIcon />
-        </S.Trigger>
-        <S.Portal>
-          <S.Overlay />
-          <S.Content>
-            <S.TopSide>
-              <S.MidSide>
-                <S.MenuButton onClick={themeToggle}>Mudar Tema</S.MenuButton>
-                <S.Divider />
-                <S.MenuItem to="/about">Quem Somos ?</S.MenuItem>
-                <S.Divider />
-                <S.MenuItemOutside href="https://github.com/matheusa1/newW2w">
-                  Código Fonte
-                </S.MenuItemOutside>
-              </S.MidSide>
-            </S.TopSide>
+	return (
+		<S.Container>
+			<S.RightSide>
+				<S.Logo to='/'>W2w?</S.Logo>
+				<S.HeaderItem to='/search'>
+					<S.HeaderName>Pesquisa</S.HeaderName>
+					<S.SearchIcon />
+				</S.HeaderItem>
+			</S.RightSide>
+			<S.Root>
+				<S.Trigger>
+					<S.MenuIcon />
+				</S.Trigger>
+				<S.Portal>
+					<S.Overlay />
+					<S.Content>
+						<S.TopSide>
+							<S.MidSide>
+								<S.MenuButton onClick={themeToggle}>Mudar Tema</S.MenuButton>
+								<S.Divider />
+								<S.MenuItem to='/about'>Quem Somos ?</S.MenuItem>
+								<S.Divider />
+								<S.MenuItemOutside
+									target={'_blank'}
+									href='https://github.com/matheusa1/newW2w'
+								>
+									Código Fonte
+								</S.MenuItemOutside>
+							</S.MidSide>
+						</S.TopSide>
 
-            <S.Close type="button">Fechar</S.Close>
-          </S.Content>
-        </S.Portal>
-      </S.Root>
-    </S.Container>
-  );
-};
+						<S.Close type='button'>Fechar</S.Close>
+					</S.Content>
+				</S.Portal>
+			</S.Root>
+		</S.Container>
+	)
+}
 
-export default Header;
+export default Header
--- src/pages/Search/styles.ts
+++ src/pages/Search/styles.ts
@@ -1,167 +1,167 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import { DebounceInput } from "react-debounce-input";
-import { NavLink } from "react-router-dom";
-import ReactLoading from "react-loading";
+import { DebounceInput } from 'react-debounce-input'
+import { NavLink } from 'react-router-dom'
+import ReactLoading from 'react-loading'
 
 interface Buttons {
-  active: boolean;
+	active: boolean
 }
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 
-  width: 100%;
+	width: 100%;
 
-  align-items: center;
+	align-items: center;
 
-  gap: 2rem;
+	gap: 2rem;
 
-  padding: 2rem;
-`;
+	padding: 2rem;
+`
 
 export const Buttons = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-`;
+	width: 100%;
+	display: flex;
+	justify-content: space-evenly;
+`
 export const Button = styled.button<Buttons>`
-  padding: 1rem;
-  width: 20rem;
-  border-radius: 50px;
-  background: #2d4174;
+	padding: 1rem;
+	width: 20rem;
+	border-radius: 50px;
+	background: #2d4174;
 
-  color: #fff;
-  font-size: 1rem;
-  font-family: Poppins, Rubik, sans-serif;
+	color: #fff;
+	font-size: 1rem;
+	font-family: Poppins, Rubik, sans-serif;
 
-  border: none;
-  cursor: pointer;
+	border: none;
+	cursor: pointer;
 
-  border: ${(props) => (props.active ? "2px solid #66D6EF" : "none")};
+	border: ${(props) => (props.active ? '2px solid #66D6EF' : 'none')};
 
-  :hover {
-    background: #1a2a4a;
-  }
-`;
+	:hover {
+		background: #1a2a4a;
+	}
+`
 
 export const InputWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: center;
+	display: flex;
+	width: 100%;
+	flex-direction: column;
+	align-items: center;
 
-  gap: 1rem;
+	gap: 1rem;
 
-  padding: 1rem 1rem 2rem;
-`;
+	padding: 1rem 1rem 2rem;
+`
 
 export const Text = styled.h1`
-  font-size: 2rem;
-  font-weight: 500;
-`;
+	font-size: 2rem;
+	font-weight: 500;
+`
 
-export const Debounce = styled(DebounceInput)``;
+export const Debounce = styled(DebounceInput)``
 
 export const Input = styled.input`
-  outline: none;
-  padding: 1rem;
-  width: 62rem;
-  border-radius: 50px;
+	outline: none;
+	padding: 1rem;
+	width: 62rem;
+	border-radius: 50px;
 
-  color: #000;
-  font-size: 1.2rem;
-  font-weight: 500;
+	color: #000;
+	font-size: 1.2rem;
+	font-weight: 500;
 
-  @media (max-width: 69rem) {
-    width: 100%;
-  }
-`;
+	@media (max-width: 69rem) {
+		width: 100%;
+	}
+`
 
 export const Results = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
+	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(5, minmax(0, 1fr));
 
-  @media (max-width: 85rem) {
-    grid-template-columns: repeat(4, minmax(0, 1fr));
-    padding: 0;
-  }
+	@media (max-width: 85rem) {
+		grid-template-columns: repeat(4, minmax(0, 1fr));
+		padding: 0;
+	}
 
-  @media (max-width: 69rem) {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-    padding: 0;
-  }
+	@media (max-width: 69rem) {
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+		padding: 0;
+	}
 
-  @media (max-width: 34rem) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-    padding: 0;
-  }
-`;
+	@media (max-width: 34rem) {
+		grid-template-columns: repeat(2, minmax(0, 1fr));
+		padding: 0;
+	}
+`
 
 export const MovieCard = styled(NavLink)`
-  text-decoration: none;
-  display: flex;
-  flex-direction: column;
+	text-decoration: none;
+	display: flex;
+	flex-direction: column;
 
-  margin: 0 auto;
+	margin: 0 auto;
 
-  width: 17rem;
+	width: 17rem;
 
-  gap: 0.5rem;
+	gap: 0.5rem;
 
-  padding: 1rem;
+	padding: 1rem;
 
-  border-radius: 0.5rem;
+	border-radius: 0.5rem;
 
-  align-items: center;
+	align-items: center;
 
-  @media (max-width: 57rem) {
-    width: 15rem;
-  }
+	@media (max-width: 57rem) {
+		width: 15rem;
+	}
 
-  @media (max-width: 47rem) {
-    width: 13rem;
-  }
+	@media (max-width: 47rem) {
+		width: 13rem;
+	}
 
-  @media (max-width: 40rem) {
-    width: 12rem;
-  }
+	@media (max-width: 40rem) {
+		width: 12rem;
+	}
 
-  transition: all 0.2s;
+	transition: all 0.2s;
 
-  &:hover {
-    transform: scale(1.05);
-    opacity: 0.8;
-  }
-`;
+	&:hover {
+		transform: scale(1.05);
+		opacity: 0.8;
+	}
+`
 
 export const MovieImage = styled.img`
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 
-  border-radius: 0.5rem;
-  overflow: hidden;
+	border-radius: 0.5rem;
+	overflow: hidden;
 
-  object-fit: cover;
-`;
+	object-fit: cover;
+`
 
 export const MovieTitle = styled.span`
-  font-size: 1rem;
-  color: ${({ theme }) => theme.text};
-`;
+	font-size: 1rem;
+	color: ${({ theme }) => theme.text};
+`
 
 export const LoadingContainer = styled.div`
-  grid-column: 1 / -1;
-  width: 100%;
-  height: 100%;
-`;
+	grid-column: 1 / -1;
+	width: 100%;
+	height: 100%;
+`
 
 export const LoadingWrapper = styled.div`
-  width: 10%;
-  margin: 0 auto;
-`;
+	width: 10%;
+	margin: 0 auto;
+`
 
-export const LoadingComponent = styled(ReactLoading)``;
-export const LoadingLabel = styled.div``;
+export const LoadingComponent = styled(ReactLoading)``
+export const LoadingLabel = styled.div``
